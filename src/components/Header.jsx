2cb0b9dbf54ebf61327388cf00829ba5bd196abb
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-07 19:06:21 +0200

    add background images imports in Header component, all layout, add styles for bg images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-export default function Header() {}
+import bgMobile from "../assets/bg-header-mobile.svg";
+import bgDesktop from "../assets/bg-header-desktop.svg";
+
+export default function Header() {
+  return (
+    <header className="w-full bg-green-400">
+      <picture className="w-full">
+        <source media="(min-width: 768px)" srcSet={bgDesktop} />
+        <img src={bgMobile} className="h-39 w-full object-cover" alt="" />
+      </picture>
+    </header>
+  );
+}
