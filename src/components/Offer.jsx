3ceec04a3fbc423e5a225381308bd77eb9e42adb
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-07 19:47:27 +0200

    add all receiving props to Offer component

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,1 +1,7 @@
-export default function Offer() {}
+export default function Offer({ company, logo, new, featured, position, role, level, posted, contract, location, languages, tools }) {
+  return (
+    <article className="w-full px-3 py-5 rounded-md shadow-xl shadow-green-400/20 bg-white">
+      <p>{company}</p>
+    </article>
+  );
+}
