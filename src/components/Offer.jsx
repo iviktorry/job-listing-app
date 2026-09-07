c802d084a26a1b80c6b3cb3f6f70ceb0d5474051
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-07 20:30:02 +0200

    receive all props from App

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,4 +1,18 @@
-export default function Offer({ company, logo, new, featured, position, role, level, posted, contract, location, languages, tools }) {
+export default function Offer({
+  company,
+  logo,
+  isNew,
+  featured,
+  position,
+  role,
+  level,
+  posted,
+  contract,
+  location,
+  languages,
+  tools,
+}) {
+  const style = `text-green-400 py-1 px-2 rounded-sm font-bold bg-green-50`;
   return (
     <article className="w-full px-3 py-5 rounded-md shadow-xl shadow-green-400/20 bg-white">
       <p>{company}</p>
