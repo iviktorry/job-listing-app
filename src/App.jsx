08f521399f12d1aa87ff75aa963b066dab54368b
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-07 21:40:36 +0200

    change from height to min-height

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Main from "./components/Main";
 
 export default function App() {
   return (
-    <div className="h-lvh font-serif">
+    <div className="min-h-lvh font-serif">
       <Header />
       <Main />
     </div>
