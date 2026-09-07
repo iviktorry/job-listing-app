7c43a5044c5200d0053f75b21813046e07d6a72b
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-07 19:48:54 +0200

    import data.json file with data, add map for data and all props for Offer

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,26 @@
 import Offer from "./Offer";
+import data from "../../data.json";
 
 export default function Main() {
   return (
-    <main className="bg-green-50 h-full">
-      <p>main</p>
-      <Offer />
+    <main className="bg-green-50 h-full px-4 py-8 flex flex-col gap-4">
+      {data.map((item) => (
+        <Offer
+          key={item.id}
+          company={item.company}
+          logo={item.logo}
+          new={item.new}
+          featured={item.featured}
+          position={item.position}
+          role={item.role}
+          level={item.level}
+          posted={item.postedAt}
+          contract={item.contract}
+          location={item.location}
+          languages={item.languages}
+          tools={item.tools}
+        />
+      ))}
     </main>
   );
 }
