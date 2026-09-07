3aa7a2cc6d1cd04ef1c5204bf44fb81e60dda035
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-07 19:11:38 +0200

    create Offer component, add Offer to Main, add import

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,10 @@
+import Offer from "./Offer";
+
 export default function Main() {
-    return (
-        <main className="bg-green-50 h-full">
-            <p>main</p>
-        </main>
-    )
+  return (
+    <main className="bg-green-50 h-full">
+      <p>main</p>
+      <Offer />
+    </main>
+  );
 }
