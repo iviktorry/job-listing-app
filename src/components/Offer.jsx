ffb04dd7e4750094622db2faa33d8565e6939edc
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-07 21:45:40 +0200

    add relative positioned logo, add conditional logic for New and Featured features, add small symbols with code, finish all styles

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -14,34 +14,43 @@ export default function Offer({
 }) {
   const style = `text-green-400 py-1 px-2 lg:h-fit rounded-sm font-bold bg-green-50`;
   return (
-    <article className="w-full px-6 py-5 rounded-md flex flex-col gap-3 shadow-xl shadow-green-400/20 bg-white">
-      <img src={logo} className="relative -top-7" alt="logo" />
+    <article className={`w-full px-6 pb-5 lg:py-8 lg:max-w-300 lg:mx-auto lg:flex lg:gap-6 rounded-lg shadow-xl shadow-green-400/20 bg-white ${featured ? "border-l-6 border-green-400" : ""}`}>
+      <img
+        src={logo}
+        className="relative -top-6 h-12 w-12 lg:h-22 lg:w-22 lg:top-0"
+        alt="logo"
+      />
 
-      <div className="flex gap-6 font-bold">
-        <span className="text-green-400">{company}</span>
-        <div className="flex gap-2">
-          {isNew && (
-            <span className="px-3 text-sm rounded-xl bg-green-400 uppercase text-white flex items-center">
-              New!
-            </span>
-          )}
-          {featured && (
-            <span className="px-3 text-sm rounded-xl bg-black uppercase text-white flex items-center">
-              Featured
-            </span>
-          )}
+      <div className="flex flex-1 flex-col gap-3 -mt-4 lg:mt-0 lg:flex-row lg:justify-between">
+        <div className="flex flex-col gap-3 lg:gap-2">
+          <div className="flex gap-6 font-bold">
+            <span className="text-green-400">{company}</span>
+            <div className="flex gap-2">
+              {isNew && (
+                <span className="px-3 text-sm rounded-xl bg-green-400 uppercase text-white flex items-center">
+                  New!
+                </span>
+              )}
+              {featured && (
+                <span className="px-3 text-sm rounded-xl bg-black uppercase text-white flex items-center">
+                  Featured
+                </span>
+              )}
+            </div>
+          </div>
+
+          <p className="font-bold text-lg lg:text-2xl">{position}</p>
+
+          <div className="flex gap-2 text-gray-400">
+            <span>{posted}</span>
+            <span>&#8226;</span>
+            <span>{contract}</span>
+            <span>&#8226;</span>
+            <span>{location}</span>
+          </div>
         </div>
-      </div>
 
-      <p>{position}</p>
-      <div className="flex gap-2 text-gray-400">
-        <span>{posted}</span>
-        <span>&#8226;</span>
-        <span>{contract}</span>
-        <span>&#8226;</span>
-        <span>{location}</span>
-      </div>
-      <div className="h-0 border-bs border-gray-400"></div>
+        <div className="h-0 border-bs border-gray-300 lg:hidden"></div>
 
         <div className="flex gap-4 flex-wrap lg:items-center">
           <span className={`${style}`}>{role}</span>
