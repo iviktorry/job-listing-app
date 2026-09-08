c75e056505a52f89c36c662751d8039b959a56d6
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-08 15:28:39 +0200

    change conditional rendering to a static rendering of filteredData array

--- src/components/OffersList.jsx
+++ src/components/OffersList.jsx
@@ -13,45 +13,25 @@ export default function OffersList({ data, handleClick, style, filters }) {
 
   return (
     <div className="flex flex-col gap-10 justify-center">
-      {filters.length === 0
-        ? data.map((item) => (
-            <Offer
-              key={item.id}
-              company={item.company}
-              logo={item.logo}
-              isNew={item.new}
-              featured={item.featured}
-              position={item.position}
-              posted={item.postedAt}
-              contract={item.contract}
-              location={item.location}
-              level={item.level}
-              role={item.role}
-              languages={item.languages}
-              tools={item.tools}
-              handleClick={handleClick}
-              style={style}
-            />
-          ))
-        : filteredData.map((item) => (
-            <Offer
-              key={item.id}
-              company={item.company}
-              logo={item.logo}
-              isNew={item.new}
-              featured={item.featured}
-              position={item.position}
-              posted={item.postedAt}
-              contract={item.contract}
-              location={item.location}
-              level={item.level}
-              role={item.role}
-              languages={item.languages}
-              tools={item.tools}
-              handleClick={handleClick}
-              style={style}
-            />
-          ))}
+      {filteredData.map((item) => (
+        <Offer
+          key={item.id}
+          company={item.company}
+          logo={item.logo}
+          isNew={item.new}
+          featured={item.featured}
+          position={item.position}
+          posted={item.postedAt}
+          contract={item.contract}
+          location={item.location}
+          level={item.level}
+          role={item.role}
+          languages={item.languages}
+          tools={item.tools}
+          handleClick={handleClick}
+          style={style}
+        />
+      ))}
     </div>
   );
 }
