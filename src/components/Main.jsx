d9f71110f578c0dc4e72c5d8c6a4e0030c97fbd9
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-08 12:35:28 +0200

    move style variable to Main, pass it to children components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import data from "../../data.json";
 import { useState } from "react";
 import OffersList from "./OffersList";
+import Filters from "./Filters";
 
 export default function Main() {
   const [filters, setFilters] = useState([]);
@@ -13,12 +14,13 @@ export default function Main() {
       return [...uniqueArr];
     });
   }
+  const style = `text-green-400 py-1 px-2 lg:h-fit rounded-sm font-bold bg-green-50 hover:bg-green-400 hover:text-green-50`;
 
-  console.log(filters);
   return (
     <main className="bg-green-50 h-full px-6">
+      <Filters filters={filters} style={`${style}`} />
       <div className="lg:max-w-300 lg:mx-auto py-8 lg:py-20">
-        <OffersList data={data} handleClick={handleClick} />
+        <OffersList data={data} handleClick={handleClick} style={`${style}`} />
       </div>
     </main>
   );
