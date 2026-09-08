8228d1bfe5d4397ee6a12e039e01eceafb62b3f8
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-08 11:56:39 +0200

    create state to save array of selected filters, finish function to add only unique element to state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,20 @@
 import Offer from "./Offer";
 import data from "../../data.json";
+import { useState } from "react";
 
 export default function Main() {
+  const [filters, setFilters] = useState([]);
+
   function handleClick(event) {
     const element = event.currentTarget.value;
-    console.log(element);
+    setFilters((prev) => {
+      const newArr = [...prev, element];
+      const uniqueArr = new Set(newArr);
+      return [...uniqueArr];
+    });
   }
+
+  console.log(filters);
   return (
     <main className="bg-green-50 h-full px-6 py-8 flex flex-col gap-10 justify-center">
       {data.map((item) => (
