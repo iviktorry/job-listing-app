4fe9a0fe94e3a492b48a4934fc8b2d643cddfeb7
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-08 12:38:18 +0200

    add map to display all clicked filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,1 +1,11 @@
-export default function Filters() {}
+export default function Filters({ filters, style }) {
+  return (
+    <div className="relative -top-8 flex gap-4 items-center flex-wrap px-4 py-5 lg:h-18  rounded-lg shadow-xl shadow-green-400/20 bg-white">
+      {filters.map((item, index) => (
+        <span key={index} className={`${style} h-fit`}>
+          {item}
+        </span>
+      ))}
+    </div>
+  );
+}
