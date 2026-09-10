f9f1b501ab3cab5f3dbe16fde3fa66c9f15d533c
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-10 13:02:12 +0200

    add aria-label to better accessibility

--- src/components/OfferDescriptionButton.jsx
+++ src/components/OfferDescriptionButton.jsx
@@ -1,6 +1,6 @@
 export default function OfferDescriptionButton({ item, style, handleClick }) {
   return (
-    <button onClick={handleClick} value={item} className={`${style} rounded-sm  hover:bg-green-400 hover:text-green-50`}>
+    <button onClick={handleClick} value={item} aria-label={`Filter by ${item}`} className={`${style} rounded-sm  hover:bg-green-400 hover:text-green-50`}>
       {item}
     </button>
   );
